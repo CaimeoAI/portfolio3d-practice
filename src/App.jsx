21defb3181aac3be4b566e1a9dde0b3526fedb4e
CaimeoAI <judiellezinke@yahoo.de>
2025-01-20 12:41:09 +0100

    added necessary imports to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,6 @@
+import { BrowserRouter } from "react-router-dom";
+import { About, Contact, Experience, Feedbacks, Hero, Navbar, Tech, Works, StarCanvas} from './components';
+
 const App = () => {
   return (
     <div>
